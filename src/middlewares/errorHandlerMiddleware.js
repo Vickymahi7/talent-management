807a14a5458b19038ed523f05ef667427c743cc2
Vignesh <vickyruv@gmail.com>
2023-10-05 10:09:55 +0530

    Update errorHandlerMiddleware.js

--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -2,7 +2,7 @@ const express = require("express");
 const responseCodes = require('../constants/httpResponseCodes.js');
 
 const errorHandler = (err, req, res, next) => {
-  const status = err.status || httpResponseCodes.INTERNAL_SERVER_ERROR;
+  const status = err.status || responseCodes.INTERNAL_SERVER_ERROR;
   const message = err.message || "Internal Server Error";
   res.status(status).json({ status: status, message: message });
 };
