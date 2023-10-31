707f1ae932b90058f4497a40e163220d9759a906
Vignesh <vickyruv@gmail.com>
2023-10-31 16:56:32 +0530

    dist remove

--- dist/middlewares/notFoundHandlerMiddleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errors_1 = require("../utils/errors");
-const notFoundHandler = (req, res, next) => {
-    const error = new errors_1.HttpNotFound("Not found");
-    next(error);
-};
-exports.default = notFoundHandler;
